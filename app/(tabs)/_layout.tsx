4987e8801532207a0d7628e2ccdc31e83b539ca4
Ali Selimi <159050119+SelimiAli@users.noreply.github.com>
2025-04-01 14:39:13 +0200

    Initial commit GIt Generated by create-expo-app 3.3.0.

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Tabs} from "expo-router";
+
+const _Layout = () => {
+    return (
+        <Tabs>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Home',
+                    headerShown: false
+                }}
+            />
+        </Tabs>
+    )
+}
+export default _Layout;
---
+++ app/movies/[id].tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native'
+import React from 'react'
+
+const MoviesDetails = () => {
+    return (
+        <View>
+            <Text>MovieDetails</Text>
+        </View>
+    )
+}
+export default MoviesDetails
